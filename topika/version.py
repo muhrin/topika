7ab7b0161a88bb40fad5299a1186d7fdbfcc795e
Martin Uhrin <martin.uhrin@gmail.com>
2018-10-04 14:53:28 +0200

    Release 0.1.3

--- topika/version.py
+++ topika/version.py
@@ -9,7 +9,7 @@ author_info = (('Martin Uhrin', 'martin.uhrin@gmail.com'),)
 package_info = "A backport of aio-pika for Tornado to support python 2.7+."
 package_license = "Apache Software License"
 
-version_info = (0, 1, 2)
+version_info = (0, 1, 3)
 
 __author__ = ", ".join("{} <{}>".format(*info) for info in author_info)
 __version__ = ".".join(tuple(map(str, version_info)))
