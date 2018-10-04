7aa111c76ad3b4b23af82d5b1cfc3906fcac43c6
Martin Uhrin <martin.uhrin@gmail.com>
2018-10-04 14:52:54 +0200

    Excluded version file for having to conform to lint variable names

--- topika/version.py
+++ topika/version.py
@@ -1,4 +1,9 @@
 from __future__ import absolute_import
+
+# Disable variable name conventions because otherwise all the variables
+# in this file would have to be upper case
+# pylint: disable=invalid-name
+
 author_info = (('Martin Uhrin', 'martin.uhrin@gmail.com'),)
 
 package_info = "A backport of aio-pika for Tornado to support python 2.7+."
